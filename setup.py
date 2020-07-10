33d80f0d71cdeaa15c649fb1b0238e9dc43dc4ef
phseiff <philipp-seiffert@gmx.de>
2020-07-10 13:22:47 +0200

    Add command line interface

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='gh_md_to_html',
     version='1.0',
     description='Github-flavored Markdown to html python and command line interface.',
+    long_description=open('README.md').read(),
+    scripts=['./scripts/gh_md_to_html'],
     author='phseiff',
     author_email='contact@phseiff.com',
     url='https://github.com/phseiff/github-flavored-markdown-to-html/',
     packages=['gh_md_to_html'],
     package_dir={'gh_md_to_html': 'src'},
     package_data={'gh_md_to_html': ['*']},
-    include_package_data=True
+    include_package_data=True,
+    license="LICENSE.txt",
+    extras_require={
+        'pdf_export': ["pdfkit"]
+    }
 )
