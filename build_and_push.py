a7bad8a1675c39bada020202149b5aa20377c9dd
phseiff <philipp-seiffert@gmx.de>
2020-09-10 02:00:55 +0200

    'test-commit-message.'

--- build_and_push.py
+++ build_and_push.py
@@ -7,6 +7,7 @@ import subprocess
 import sys
 
 for command in [
+    "sudo python3 uninstall gh-md-to-html",
     "sudo python3 setup.py install",
     "gh-md-to-html --help",
     "git add *",
@@ -16,4 +17,9 @@ for command in [
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     if command.startswith("gh-md-to-html"):
+        with open("src/help.txt", "wb") as help_text_file:
+            help_text_file.write(output)
+            with open("README-raw.md", "r") as raw_readme_file:
+                with open("README.md", "w") as readme_file:
+                    readme_file.write(raw_readme_file.read().replace("{help}", str(output, encoding="UTF-8")))
         print("{" + str(output, encoding="UTF-8") + "}")
