271ef3472c948a09f6ed772e9ff729bfe718d788
phseiff <philipp-seiffert@gmx.de>
2020-09-10 03:28:05 +0200

    'test-commit-message.'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='gh_md_to_html',
-    version='1.0.5',
+    version='1.1.0',
     description='Github-flavored Markdown to html python and command line interface.',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
