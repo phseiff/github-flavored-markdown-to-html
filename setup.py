2c5b9e81b27f2768519f5634f8a058eb8617962a
phseiff <phseiff@phseiff.com>
2020-12-05 17:00:18 +0100

    Create new release to update contact email address

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name='gh_md_to_html',
-    version='1.6.4',
+    version='1.6.5',
     description='Github-flavored Markdown to html python and command line interface.',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     scripts=['./scripts/gh-md-to-html'],
     author='phseiff',
-    author_email='contact@phseiff.com',
+    author_email='phseiff@phseiff.com',
     url='https://github.com/phseiff/github-flavored-markdown-to-html/',
     packages=['gh_md_to_html'],
     package_dir={'gh_md_to_html': 'src'},
